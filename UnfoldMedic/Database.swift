ea53131d1248d7b3711463f48e42914072052cae
Ruannilton <student@student24.local>
2019-03-25 18:34:01 -0300

    Bessa Pediu

--- UnfoldMedic/Database.swift
+++ UnfoldMedic/Database.swift
@@ -8,17 +8,17 @@
 
 import Foundation
 
-enum DataType{
-   case Alergia,Doenca,Anamnese,Cirurgia,DoencaFisica,Vacina
+enum CardType{
+   case Alergia,Doenca,Anamnese,Cirurgia,DoencaFisica,Vacina,Medicamentos
 }
 
 class DadosCard{
 
     var nome:String
     var subs:String
-    var tipo:DataType
+    var tipo:CardType
     
-    init(nome:String,subs:String,tipo:DataType){
+    init(nome:String,subs:String,tipo:CardType){
         self.nome = nome
         self.subs = subs
         self.tipo = tipo
@@ -28,6 +28,18 @@ class DadosCard{
 
 class Database{
 
+    public static func getDadosCard()->[DadosCard]{
+    return[
+        DadosCard(nome:"Alergias",subs:"Lista de suas alergias",tipo:CardType.Alergia),
+        DadosCard(nome:"Doenças",subs:"Lista de suas doenças",tipo:CardType.Doenca),
+        DadosCard(nome:"Medicamentos",subs:"Lista de medicamentos já tomados",tipo:CardType.Medicamentos),
+        DadosCard(nome:"Anamnese",subs:"Lista de suas alergias",tipo:CardType.Anamnese),
+        DadosCard(nome:"Cirurgia",subs:"Cirurgias passadas",tipo:CardType.Cirurgia),
+        DadosCard(nome:"Doenças Físicas",subs:"Lista de suas doenças físicas",tipo:CardType.DoencaFisica),
+        DadosCard(nome:"Vacinas",subs:"Lista das vacinas já tomadas",tipo:CardType.Vacina)
+        
+        ]
     
+    }
     
 }
